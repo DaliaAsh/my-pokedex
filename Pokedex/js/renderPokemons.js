9b5eb390f24110493b16ab02b6f8743e53743ec7
Dalia <dalia@kiitos-tech.com>
2021-02-18 21:10:28 +0200

    build the basic design

--- Pokedex/js/renderPokemons.js
+++ Pokedex/js/renderPokemons.js
@@ -1,5 +1,13 @@
-function renderPokemonCard(pokemonData) {
-  var pokemonDataParsed = JSON.parse(pokemonData);
-  console.log(pokemonDataParsed.name);
-  console.log(pokemonDataParsed.imageUrl);
+function transformPokemonsToCards(pokemonsArray) {
+  console.log(pokemonsArray);
+  let html = "";
+  pokemonsArray.map((pokemon) => {
+    html += `<div class="card" style="margin:2em;width:18em">
+    <div class="card-title">${pokemon.name}</div>
+    <div class="">
+    <img alt="${pokemon.name}" src="${pokemon.frontImageUrl}"/>
+    </div>
+    </div>`;
+  });
+  document.getElementById("container").innerHTML = html;
 }
